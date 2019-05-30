fd4bb4ca9b2025cae9bb1f8a215e5e02be6a67c2
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 14:25:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,7 +55,9 @@ class Owner
   end
 
   def sell_pets
-    @pets = []
+    @pets[:dogs].each {|dog| dog.mood = "nervous"}
+    @pets[:cats].each {|cat| cat.mood = "nervous"}
+    @pets[:fishes].each {|fish| fish.mood = "nervous"}
   end
 
 
